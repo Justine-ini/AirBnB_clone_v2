04fb855cd173651209a3d14e8b5f38a8d07e8ccd
Justine-ini <inijustine4040@gmail.com>
2023-12-30 05:08:48 -0800

    fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""A script that starts a flask web application
+"""
+A script that starts a Flask web application
 """
 
 from flask import Flask
+
 app = Flask(__name__)
 
 
-app.route('/', strict_slashes=False)
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    Displays 'Hello HBNB!'
+    """
+    return "Hello HBNB!"
 
 
-def display():
-    """Displays 'Hello HBNB!'"""
-    return (“Hello HBNB!”)
-
-
-if __name__ == __main__:
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=None)
